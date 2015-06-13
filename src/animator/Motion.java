e59b340a6b54c6d95a2b2b286123fff8a0c33ec5
nbatista <nbatista@inf.ufsm.br>
2015-06-13 19:45:17 -0300

    .

---
+++ src/animator/Motion.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package animator;
+
+import java.awt.Dimension;
+
+/**
+ *
+ * @author nbati_000
+ */
+public class Motion {
+    double rout;
+    int i;
+    int j;
+    Dimension D;
+
+    public Motion(int i, int j, Dimension D) {
+        this.i = i;
+        this.j = j;
+        this.D = D;
+        rout = 1.2;
+    }
+    
+    public Motion(int[] a){
+        i = a[0];
+        j = a[1];
+    }
+    
+   
+    
+    public int linear(int i, int j){
+        if (i >  (int)(D.getWidth())){
+            return -40;
+        }
+        i += 10;
+        return i;
+    }
+}
